2defe574c29917a626d4da02ec36dd2cc45246d7
Etienne CASSIN <ecassin@campus-eni.fr>
2025-07-16 12:27:03 +0200

    rest repositories

--- src/main/java/fr/eni/demospringwebservices/restrepositories/CocktailRestRepository.java
+++ src/main/java/fr/eni/demospringwebservices/restrepositories/CocktailRestRepository.java
@@ -1,9 +1,14 @@
 package fr.eni.demospringwebservices.restrepositories;
 
 import fr.eni.demospringwebservices.entities.Cocktail;
+import fr.eni.demospringwebservices.entities.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "cocktails", path = "cocktails")
 public interface CocktailRestRepository extends JpaRepository<Cocktail, Integer> {
+    List<Cocktail> findByNomContains(String nom);
+    List<Cocktail> findByAlcooliseIs(Boolean alcoolise);
 }
